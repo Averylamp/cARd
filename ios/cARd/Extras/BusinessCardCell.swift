e237a77dba7cafc68e54290c148b1d48fe50a096
RyanNavillus <ryanpnavillus@gmail.com>
2018-09-15 16:29:27 -0400

    Polish History UI

--- ios/cARd/Extras/BusinessCardCell.swift
+++ ios/cARd/Extras/BusinessCardCell.swift
@@ -16,12 +16,55 @@ class BusinessCardCell: UITableViewCell {
     @IBOutlet weak var timestampLabel: UILabel!
     
     @IBOutlet weak var buttonScrollView: UIScrollView!
-    @IBOutlet weak var buttonStackView: UIView!
+    @IBOutlet weak var buttonContentView: UIView!
+    @IBOutlet weak var buttonStackView: UIStackView!
+    
+    let links: [String] = ["Lol", "Wow", "Much Testing"]
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
         
+        containerView.layer.cornerRadius = 10
+        containerView.clipsToBounds = true
+        containerView.layer.shadowOffset = CGSize(width: 2, height: 5)
+        let shadowPath = UIBezierPath(rect: CGRect(x: 0, y: 0, width: containerView.bounds.width-20, height: containerView.bounds.height))
+        containerView.layer.masksToBounds = false
+        containerView.layer.shadowColor = UIColor.black.cgColor
+        containerView.layer.shadowOpacity = 0.5
+        containerView.layer.shadowPath = shadowPath.cgPath
+        
+        pictureView.backgroundColor = .red
+        pictureView.layer.cornerRadius = pictureView.frame.width/2
+        pictureView.clipsToBounds = true
+        pictureView.layer.borderWidth = 1
+        pictureView.layer.borderColor = UIColor.darkGray.cgColor
+        
+        buttonScrollView.contentSize = buttonContentView.frame.size
+        
+        setupButtonStack()
+    }
+    
+    func setupButtonStack() {
+        for view in buttonStackView.arrangedSubviews {
+            view.removeFromSuperview()
+        }
+        for link in links {
+            let button = UIButton(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+            
+            button.translatesAutoresizingMaskIntoConstraints = false
+            
+            button.heightAnchor.constraint(equalTo: button.widthAnchor).isActive = true
+            button.setTitle("", for: .normal)
+            button.backgroundColor = .orange
+            
+            button.clipsToBounds = true
+            button.layer.cornerRadius = button.frame.width/2
+            button.layer.borderColor = UIColor.blue.cgColor
+            button.layer.borderWidth = 2
+            
+            buttonStackView.addArrangedSubview(button)
+        }
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
