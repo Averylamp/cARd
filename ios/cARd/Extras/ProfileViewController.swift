4bb0529728e3f73a7595c91f13f5baf592af6809
RyanNavillus <ryanpnavillus@gmail.com>
2018-09-16 03:04:37 -0400

    Add SDWebImage

--- ios/cARd/Extras/ProfileViewController.swift
+++ ios/cARd/Extras/ProfileViewController.swift
@@ -7,12 +7,12 @@
 //
 
 import UIKit
+import SDWebImage
 
 class ProfileViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
     
