c1217c146189a701eb75a0f077785db75b27d8f2
Moin Nadeem <mnadeem@mit.edu>
2018-09-16 01:31:48 -0400

    adding final version

--- server/scraping_util.py
+++ server/scraping_util.py
@@ -1,4 +1,6 @@
+import base64
 import re
+import requests
 
 from MAEZ.scrapers.Google import GoogleScraper
 from MAEZ.scrapers.LinkedIn import LinkedInScraper
@@ -6,16 +8,15 @@ from MAEZ.scrapers.LinkedIn import LinkedInScraper
 def get_person(name, phone_number, email):
     g = GoogleScraper()
     linkedin_urls = g.scrape_by_name(user=name)
-    print(linkedin_urls)
-    name_no_linkedin = name.split("linkedin")[0]
-    all_urls = g.scrape_by_name(user=name_no_linkedin)
 
     l = LinkedInScraper()
     l.sign_in()
-    l.navigate(linkedin_urls['linkedin'][0])
+    l.navigate(all_urls['linkedin'][0])
     l.driver.save_screenshot("test")
     p = l.extract_profile()
 
+    if p.profile_picture is not None:
+        picture = get_as_base64(p.profile_picture)
     response = {}
     response['name'] = "{} {}".format(p.first, p.last)
     response['education_list'] = p.educationalHistory
@@ -25,6 +26,10 @@ def get_person(name, phone_number, email):
     response['position_str'] = "Previously worked at {}.".format(s)
     response['description'] = p.headline
     response['links'] = all_urls
-    response['phone_number'] = re.sub("[^0-9]", phone_number)
+    response['phone_number'] = re.sub("[^0-9]", "", str(phone_number))
     response['email'] = email
+    response['profile_picture'] = picture
     return response
+
+def get_as_base64(url):
+    return base64.b64encode(requests.get(url).content)
